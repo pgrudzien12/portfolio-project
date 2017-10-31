48a3c5bac32ca592e356563df69b4994340936a9
Urszula Grudzien <ugrudzien12@gmail.com>
2017-10-31 20:59:15 +0100

    Add file

---
+++ strona/js/script.js
@@ -0,0 +1,62 @@
+//curent date
+function setDate() {
+    var date = new Date();
+
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    var today = year + "-" + month + "-" + day;
+
+    document.getElementById("datepicker").value = today;
+}
+
+
+//detapicker
+
+$('#datetimepicker2').datepicker({
+    format: "yyyy-mm-dd",
+    weekStart: 1,
+    todayBtn: "linked",
+    keyboardNavigation: false,
+    forceParse: false,
+    autoclose: true,
+    daysOfWeekHighlighted: "0,6",
+    todayHighlight: true,
+    beforeShowYear: function (date) {
+        if (date.getFullYear() == 2007) {
+            return false;
+        }
+    }
+
+});
+
+//blocking input worker time input when option were selected
+function chooseTimeWrittenBlock() {
+    var workerTime = document.getElementById('workerWrittenTime');
+    //var select = document.getElementById("timeSelect");
+workerTime.disabled=true;
+   // workerTime.disabled = true;
+    
+     ////   if (document.getElementById("selectOption").click = true) {
+
+
+          //  workerTime.disabled = false;
+        
+   // }
+}
+
+
+    function writtenTimeSelectBlock() {
+       //// var workerTime = document.getElementById("workerWrittenTime");
+       // workerTime.onchange = function () {
+           if ($("#workerWrittenTime").is(":checked") ){
+                $("timeSelect").prop("disable", "disable");
+            }
+       }
+   // }
+
+
